e5978861f053e798b6777a7cef0bbc9fdee56f3c
MunHammer <191528445+MunHammer@users.noreply.github.com>
2026-06-04 17:29:53 +1200

    Made the framework for the cli frontend

---
+++ src/main.rs
@@ -0,0 +1,38 @@
+#![doc = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/README.md"))]
+#[cfg(feature = "cli")]
+mod cli {
+    #[allow(clippy::wildcard_imports)]
+    use super::*;
+    use clap::Parser;
+    use std::path::PathBuf;
+
+    #[derive(Parser)]
+    #[command(version, about, long_about = None, propagate_version = true)]
+    struct Cli {
+        #[arg(short, long)]
+        file: Option<PathBuf>,
+        #[arg(short = 'L', long)]
+        long_license: bool,
+        #[arg(short = 'l', long)]
+        short_license: bool,
+    }
+    pub(crate) fn run() {
+        let cli = Cli::parse();
+        if cli.long_license {
+            println!(
+                "{}",
+                include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/LICENSE"))
+            );
+        } else if cli.short_license {
+            println!(
+                "This project is license under the MIT license, a permissive software license."
+            );
+        }
+    }
+}
+fn main() {
+    #[cfg(not(feature = "cli"))]
+    compile_error!("Can't build binary without cli feature");
+    #[cfg(feature = "cli")]
+    cli::run();
+}
